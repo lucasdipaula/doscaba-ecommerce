c51522d574f381add6089cea096b884a9214896c
lucasdipaula <lucas.dipaula9969@gmail.com>
2023-05-30 01:22:41 -0300

    chore: Initialize project

---
+++ src/main/java/com/doscabaecommerce/DoscabaecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.doscabaecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoscabaecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoscabaecommerceApplication.class, args);
+	}
+
+}
